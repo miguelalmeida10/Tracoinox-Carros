f49081002cf0ccf8cce6ec5ca534fd3de0f83191
miguelalmeida10 <33085795+miguelalmeida10@users.noreply.github.com>
2022-01-31 16:55:01 +0000

    Initial

---
+++ GestorVeiculos/Data/Models/Activity.cs
@@ -0,0 +1,10 @@
+﻿namespace GestorVeiculos.Data.Models
+{
+    public class Activity
+    {
+        public int ActivityID { get; set; }
+        public Car CarID { get; set; }
+        public int? AddedKm { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
---
+++ GestorVeiculos/Data/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace GestorVeiculos.Data.Models
+{
+    public class Car
+    {
+        public string? CarName { get; set; }
+        public string? Plate { get; set; }
+        
+        public DateTime InspectionDate { get; set; }
+        public int KilometersDone { get; set; }
+        public int KilometersLimit { get; set; }
+        public int TiresLimit { get; set; }
+        public int TiresDone { get; set; }
+        public bool AlertMessage { get; set; }
+    }
+}
